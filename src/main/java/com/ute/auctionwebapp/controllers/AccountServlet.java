27acf972673e6191dff7ebceb95c7897f59ea5dd
waflol <lenguyenhunganh2001@gmail.com>
2021-12-24 22:07:43 +0700

    Add AccountServlet

---
+++ src/main/java/com/ute/auctionwebapp/controllers/AccountServlet.java
@@ -0,0 +1,71 @@
+package com.ute.auctionwebapp.controllers;
+
+
+//import at.favre.lib.crypto.bcrypt.BCrypt;
+
+import com.ute.auctionwebapp.Utils.ServletUtils;
+import com.ute.auctionwebapp.beans.Category;
+import com.ute.auctionwebapp.beans.Product;
+import com.ute.auctionwebapp.beans.SubCategory;
+import com.ute.auctionwebapp.beans.User;
+import com.ute.auctionwebapp.models.CategoryModel;
+import com.ute.auctionwebapp.models.ProductModelAdmin;
+import com.ute.auctionwebapp.models.SubCategoryModel;
+import com.ute.auctionwebapp.models.UserModel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@WebServlet(name = "AccountServlet", value = "/Account/*")
+public class AccountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getPathInfo();
+
+        switch (path) {
+            case "/Register":
+                ServletUtils.forward("/views/vwAccount/Register.jsp", request, response);
+                break;
+            case "/Login":
+                ServletUtils.forward("/views/vwAccount/Login.jsp", request, response);
+                break;
+            case "/Profile":
+                ServletUtils.forward("/views/vwAccount/Profile.jsp", request, response);
+                break;
+            default:
+                ServletUtils.forward("/views/404.jsp", request, response);
+                break;
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getPathInfo();
+        switch (path) {
+            case "/Register":
+                registerUser(request, response);
+                break;
+            case "/Login":
+                loginUser(request, response);
+                break;
+            default:
+                ServletUtils.forward("/views/404.jsp", request, response);
+                break;
+        }
+    }
+    private void registerUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    private void loginUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
+
